80a33c9ed71ad57953a62cd150eff9335b906cf6
Eduardo Henrique <duardohenriqe19@gmail.com>
2023-07-30 19:05:47 -0300

    validação formulario

--- js/script.js
+++ js/script.js
@@ -31,4 +31,64 @@ function initModal() {
   }
 }
 
+function initValidateForm() {
+  let nameInput = document.querySelector("#name");
+  let emailInput = document.querySelector("#email");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let messageText = document.querySelector("#message");
+
+  const errorName = document.querySelector(".error-name");
+  const errorEmail = document.querySelector(".error-email");
+  const errorMessage = document.querySelector(".error-message");
+  const btnSubmit = document.querySelector(".btn-submit");
+
+  if (nameInput && emailInput && messageText) {
+    function resetStyles() {
+      errorName.classList.remove("active");
+      nameInput.classList.remove("error-focus", "sucess-focus");
+      errorEmail.classList.remove("active");
+      emailInput.classList.remove("error-focus", "sucess-focus");
+      errorMessage.classList.remove("active");
+      messageText.classList.remove("error-focus", "sucess-focus");
+    }
+
+    function validateForm(event) {
+      event.preventDefault();
+      resetStyles();
+
+      if (messageText.value === "") {
+        errorMessage.classList.add("active");
+        messageText.classList.add("error-focus");
+        messageText.focus();
+      } else {
+        errorMessage.classList.remove("active");
+        messageText.classList.remove("error-focus");
+        messageText.classList.add("sucess-focus");
+      }
+
+      if (!emailInput.value.match(emailRegex)) {
+        errorEmail.classList.add("active");
+        emailInput.classList.add("error-focus");
+        emailInput.focus()
+      } else {
+        errorEmail.classList.remove("active");
+        emailInput.classList.remove("error-focus");
+        emailInput.classList.add("sucess-focus");
+      }
+
+      if (nameInput.value === "") {
+        errorName.classList.add("active");
+        nameInput.classList.add("error-focus");
+        nameInput.focus();
+      } else {
+        errorName.classList.remove("active");
+        nameInput.classList.remove("error-focus");
+        nameInput.classList.add("sucess-focus");
+      }
+    }
+    btnSubmit.addEventListener("click", validateForm);
+  }
+}
+
 initModal();
+initValidateForm();
